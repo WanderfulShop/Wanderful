1d83b689b97587524d8049f22940e1a6ed250505
Brittney Brinson <brittneyc95@outlook.com>
2023-01-12 10:15:48 -0500

    Got the profile page to pull from DB and added visuals

--- src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
@@ -17,17 +17,18 @@ import java.util.List;
 //get info from the login servlet
 @WebServlet(name = "controllers.ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         if (request.getSession().getAttribute("user") == null && request.getSession().getAttribute("password") == null) {
             response.sendRedirect("/login");
             return;
         }
-        Ads adsDao = DaoFactory.getAdsDao();
-        List<Ad> ad = adsDao.all();
-        request.setAttribute("ads",ad);
+        //With the request I am SETTING the attribute with my desired input (all the ads) and PASSING it to the JSP with the help of the DISPATCHER
+        request.setAttribute("ads", DaoFactory.getAdsDao().all());
         request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
 
     }
+
 }
 
 
