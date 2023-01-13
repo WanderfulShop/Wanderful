1c92a7d2f0a7667a6e4a80775ecb859b37699c76
Miles Raker <milesraker@gmail.com>
2023-01-12 22:52:29 -0800

    before using new SQL migration and seeder files

--- migration.sql
+++ migration.sql
@@ -1,27 +1,31 @@
 CREATE DATABASE IF NOT EXISTS wanderful_db;
-SHOW DATABASES;
+
 USE wanderful_db;
+
 CREATE TABLE users (
-id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-user_name VARCHAR(100) NOT NULL UNIQUE,
-first_name VARCHAR(100) NOT NULL,
-last_name VARCHAR(100) NOT NULL,
-school VARCHAR(100),
-email VARCHAR(100) NOT NULL,
-PRIMARY KEY (id)
+    user_password VARCHAR(100) NOT NULL,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_name VARCHAR(100) NOT NULL UNIQUE,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    school VARCHAR(100),
+    email VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
 );
+
 CREATE TABLE wands (
-id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-wand_name VARCHAR(50) NOT NULL,
-core_material VARCHAR(80) NOT NULL,
-wood_type VARCHAR(80) NOT NULL,
-use_category VARCHAR(80) NOT NULL,
-age INT NOT NULL,
-image VARCHAR(100),
-user_id int UNSIGNED NOT NULL,
-PRIMARY KEY (id),
-FOREIGN KEY (user_id) REFERENCES users(id)
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    wand_name VARCHAR(50) NOT NULL,
+    core_material VARCHAR(80) NOT NULL,
+    wood_type VARCHAR(80) NOT NULL,
+    use_category VARCHAR(80) NOT NULL,
+    age INT NOT NULL,
+    image VARCHAR(100),
+    user_id int UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
 CREATE TABLE ads (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     creation_date DATE NOT NULL,
@@ -29,19 +33,14 @@ CREATE TABLE ads (
     ad_description VARCHAR(200) NOT NULL,
     PRIMARY KEY(id)
 );
+
 CREATE TABLE wand_ads (
     wand_id INT UNSIGNED NOT NULL,
     ad_id INT UNSIGNED NOT NULL,
     FOREIGN KEY (wand_id) REFERENCES wands(id),
     FOREIGN KEY (ad_id) REFERENCES ads(id)
 );
-DESCRIBE users;
-ALTER TABLE users
-ADD user_password VARCHAR(100) FIRST;
-DESCRIBE users;
-ALTER TABLE users MODIFY user_password VARCHAR(100 )NOT NULL FIRST;
-DESCRIBE users;
-USE wanderful_db;
+
 
 
 
